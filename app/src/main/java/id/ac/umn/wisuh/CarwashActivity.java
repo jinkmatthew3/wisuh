2a263758333f8c18f21bce7c44a814aa34e452af
Michael <michaelrony17@gmail.com>
2020-05-04 00:53:40 +0700

    Firebase Get Carwash NAME + SIZE

--- app/src/main/java/id/ac/umn/wisuh/CarwashActivity.java
+++ app/src/main/java/id/ac/umn/wisuh/CarwashActivity.java
@@ -2,7 +2,10 @@ package id.ac.umn.wisuh;
 
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -12,6 +15,23 @@ import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintSet;
 
@@ -19,16 +39,68 @@ public class CarwashActivity extends AppCompatActivity {
     LinearLayout llayout;
     private int btni;
 
+    //Firebase Firestore
+    private FirebaseFirestore db;
+    ArrayList<String> listCarwash;
+    ArrayList<Image> listFotoCarwash;
+
+    StorageReference storageReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carwash);
 
+        //bikin arraylist
+        listCarwash = new ArrayList<>();
+        listFotoCarwash = new ArrayList<>();
+
+        //ambil database
+        db = FirebaseFirestore.getInstance();
+
+        //ambil data dari koleksi Carwash firestore
+        db.collection("Carwash")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                //Log.d("testingCarwash", document.getId() + " => " + document.getData());
+                                String tempString = document.getString("nama");
+                                //Log.d("testingCarwash",tempString);
+                                listCarwash.add(tempString);
+                                /*storageReference = FirebaseStorage.getInstance().getReference();
+                                StorageReference profilRef = storageReference.child("carwash/"+document.getId()+"/profil.jpg");
+                                profilRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        Image tempImage = null;
+                                        Picasso.get().load(uri).into((Target) tempImage);
+                                        listFotoCarwash.add(tempImage);
+                                    }
+                                });*/
+                            }
+                            /*for (String namaMobil : listCarwash) {
+                                Log.d("testingCarwash2",listCarwash.size()+" ");
+                                Log.d("testingCarwash2", namaMobil);
+                            }*/
+                            //bikin imageButton buat ditampilin
+                            makeButton();
+                        } else {
+                            Log.d("testingCarwash", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+
+    public void makeButton(){
         llayout = findViewById(R.id.scrollview);
         btni = 1;
-
+        Log.d("testingCarwash3",listCarwash.size()+" ");
         //Adding secara dynamic
-        for (int i = 1; i <= 7; i++) {
+        for (int i = 1; i <= listCarwash.size(); i++) {
             RelativeLayout rlayout = new RelativeLayout(this);
             RelativeLayout.LayoutParams imageButtonParam = new RelativeLayout.LayoutParams(
                     RelativeLayout.LayoutParams.MATCH_PARENT,
@@ -51,7 +123,7 @@ public class CarwashActivity extends AppCompatActivity {
             tview.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
             tview.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
             tview.setLayoutParams(imageButtonParam);
-            tview.setText("Carwhas");
+            tview.setText(listCarwash.get(i-1));
             tview.setTextSize(15);
             tview.setTypeface(Typeface.DEFAULT_BOLD);
             tview.setTypeface(Typeface.SANS_SERIF);
@@ -70,6 +142,7 @@ public class CarwashActivity extends AppCompatActivity {
             rlayout.addView(tview);
             btni = btni + 1;
         }
-
     }
 }
+
+
