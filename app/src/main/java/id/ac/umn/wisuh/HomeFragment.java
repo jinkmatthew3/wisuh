660362c4d5d0e42e3e3af227366f598721622421
Michael <michaelrony17@gmail.com>
2020-05-07 21:19:54 +0700

    Ngambil saldo yang di Fragment Home pake firebase

--- app/src/main/java/id/ac/umn/wisuh/HomeFragment.java
+++ app/src/main/java/id/ac/umn/wisuh/HomeFragment.java
@@ -2,10 +2,20 @@ package id.ac.umn.wisuh;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,10 +25,21 @@ public class HomeFragment extends Fragment {
     private ImageButton btnnearby;
     private ImageButton btnsalon;
 
+    //buat dapetin usernya
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+
+    //Firebase Firestore
+    private FirebaseFirestore db;
+
+    private TextView tvSaldo;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        tvSaldo = view.findViewById(R.id.tvSaldo);
 
         btnnearby = view.findViewById(R.id.btn_carwash);
         btnnearby.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +58,34 @@ public class HomeFragment extends Fragment {
                 startActivity(pindahsalon);
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        user = mAuth.getCurrentUser();
+
+        final DocumentReference docRef = db.collection("users").document(user.getUid());
+
+        if(user != null){
+//            docRef = db.collection("users").document(user.getUid());
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            Double saldo = document.getDouble("saldo");
+                            tvSaldo.setText("Rp. " + saldo);
+                        }
+//                        TINGGAL DI BENERIN LOGHNYA
+                        else {
+                            Log.d("signIn", "No such document");
+                        }
+                    } else {
+                        Log.d("signIn", "get failed with ", task.getException());
+                    }
+                }
+            });
+        }
 
         return view;
     }
