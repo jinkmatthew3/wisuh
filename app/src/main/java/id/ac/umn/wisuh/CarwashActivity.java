aba56e558401b203e2d3ff030d6e34a87c78b9b6
dhammajoti <dhammajoti@student.umn.ac.id>
2020-05-11 05:45:26 +0700

    edit biar kalo saldo kurang ga bisa finish

--- app/src/main/java/id/ac/umn/wisuh/CarwashActivity.java
+++ app/src/main/java/id/ac/umn/wisuh/CarwashActivity.java
@@ -11,13 +11,18 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,6 +42,7 @@ import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import androidx.annotation.NonNull;
@@ -44,7 +50,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.app.ActivityCompat;
-
+//joti coba nambahin implements filterable
+//public class CarwashActivity extends AppCompatActivity implements Filterable {
 public class CarwashActivity extends AppCompatActivity {
     LinearLayout llayout;
     Toolbar toolbar;
@@ -255,6 +262,61 @@ public class CarwashActivity extends AppCompatActivity {
             }
         });
     }
+
+//    joti coba nambahin search
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.search_menu,menu);
+//        MenuItem item = menu.findItem(R.id.searchMenu);
+//        SearchView searchView = (SearchView) item.getActionView();
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                getFilter();
+//                return false;
+//            }
+//        });
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public Filter getFilter() {
+//        return filter;
+//    }
+//    Filter filter = new Filter() {
+////        run on backgorund thread
+//        @Override
+//        protected FilterResults performFiltering(CharSequence constraint) {
+//            List<String> filteredList = new ArrayList<>();
+//            if (constraint.toString().isEmpty()){
+//                filteredList.addAll(listCarwash);
+//            }
+//            else{
+//                for (String movie: listCarwash){
+//                    if(movie.toLowerCase().contains(constraint.toString().toLowerCase())){
+//                        filteredList.add(movie);
+//                    }
+//                }
+//            }
+//            FilterResults filterResults = new FilterResults();
+//            filterResults.values=filteredList;
+//
+//            return filterResults;
+//        }
+////      run on a ui  thread
+//        @Override
+//        protected void publishResults(CharSequence constraint, FilterResults results) {
+//            listCarwash.clear();
+//            listCarwash.addAll((Collection<? extends String>) results.values);
+//
+//        }
+//    };
 }
 
 
